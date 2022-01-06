0515b7839db6fb9bf716336a914583cb4f2a0275
vaishak101 <vaishak101@gmail.com>
2022-01-06 18:59:30 +0530

    IMAGE-OPTIMISED // carousel-added // - 06 jan 2021

--- js/script.js
+++ js/script.js
@@ -1,1 +1,45 @@
+//SLIDER
+const closeSlider = document.querySelector('.slider__btn--close');
+const openSlider = document.querySelectorAll('.btn-open');
+const cara_slider = document.querySelector('.popup_apt');
+const sliderbtnLeft = document.querySelector('.slider__btn--1');
+const sliderbtnRight = document.querySelector('.slider__btn--2');
+const slide = document.querySelectorAll('.slide');
+let curSlide = 0;
+let maxSlide = slide.length - 1;
 
+const slider = function (cursl) {
+  slide.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - cursl)}%)`)
+  );
+};
+const nextSlide = function () {
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  slider(curSlide);
+};
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;
+  }
+  slider(curSlide);
+};
+const hideSlider = function () {
+  cara_slider.classList.remove('visible');
+};
+const openCaraSlider = function () {
+  cara_slider.classList.add('visible');
+};
+slider(0);
+
+sliderbtnLeft.addEventListener('click', prevSlide);
+sliderbtnRight.addEventListener('click', nextSlide);
+closeSlider.addEventListener('click', hideSlider);
+for (let i = 0; i < openSlider.length; i++) {
+  openSlider[i].addEventListener('click', openCaraSlider);
+}
